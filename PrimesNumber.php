70cb04a320710089038bd79bb8ad7fe4b6dd3de6
DolbyCpp <badio12@gmail.com>
2016-03-05 03:00:25 -0300

    Print the prime numbers from 0 to 1000

---
+++ PrimesNumber.php
@@ -0,0 +1,15 @@
+<?php
+
+for($i = 2; $i <= 1000; $i++)
+{
+	$prime = true;
+	for($aux = 2; $aux < $i; $aux++)
+	{
+		if($i % $aux == 0) 
+		{
+			$prime = false;
+			break;
+		}
+	}
+	if($prime) echo $aux . " ";
+}
